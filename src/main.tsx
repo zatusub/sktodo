0248ce120525bf6a3c946d421baa0aeb0a726c8f
zatunohito <zatunohito@gmail.com>
2025-12-12 20:28:24 +0900

    add: create stripe page

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.js'
 import CalendarTestPage from './pages/CalendarTestPage'
+import MoneyPage from './pages/MoneyPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/test/calendar" element={<CalendarTestPage />} />
+        <Route path="/money" element={<MoneyPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
