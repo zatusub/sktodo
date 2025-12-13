d2c76e1ee22df01fb359bd6ed6de6699adb84e18
tga79-ctrl <tga_79@icloud.com>
2025-12-13 11:22:59 +0900

    Add AchieveButton styling and TodoDetailDisplay component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { AchieveButton } from './components/achieve/AchieveButton';
 import './App.css'
-
+import { TodoDetailDisplay } from './components/achieve/TodoDetailDisplay';
 function App() {
 
   return (
     <>
       <AchieveButton />
+      <TodoDetailDisplay />
     </>
+   
   )
 }
 
